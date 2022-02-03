cd21201259428d740308016bf4019a000f5a34cd
Ana Raquel <anaraquelpmatos@gmail.com>
2022-02-03 16:53:08 -0300

    Primeira versão (finalização do layout principal)

---
+++ script.js
@@ -0,0 +1,33 @@
+/*let cartas = null;
+let jogo = [];
+
+function perguntarUsuario() {
+ 
+   while ((cartas % 2 !== 0) || (cartas < 4)|| (cartas > 14) || (cartas === "") || (cartas === null)) {
+      cartas = parseInt(prompt("Olá! Quantas cartas você deseja para iniciar o jogo? Escolha um número par de 1 a 14!"));
+    }
+}
+perguntarUsuario()
+
+/*
+function selecionarCartas() {
+   jogo[0] = <img src="Images/bobrossparrot.gif" alt="bobrossparrot.gif" />;
+   jogo[1] = <img src="Images/metalparrot.gif" alt="metalparrot.gif" />;
+   jogo[2] = <img src="Images/explodyparrot.gif" alt="explodyparrot.gif" />;
+   jogo[3] = <img src="Images/fiestaparrotr.gif" alt="fiestaparrotr.gif" />;
+   jogo[4] = <img src="Images/revertitparrot.gif" alt="revertitparrot.gif" />;
+   jogo[5] = <img src="Images/tripletsparrot.gif" alt="tripletsparrot.gif" />;
+   jogo[6] = <img src="Images/unicornparrot.gif" alt="unicornparrot.gif" />;
+   jogo[7] = <img src="Images/bobrossparrot.gif" alt="bobrossparrot.gif" />;
+   jogo[8] = <img src="Images/metalparrot.gif" alt="metalparrot.gif" />;
+   jogo[9] = <img src="Images/explodyparrot.gif" alt="explodyparrot.gif" />;
+   jogo[10] = <img src="Images/fiestaparrotr.gif" alt="fiestaparrotr.gif" />;
+   jogo[11] = <img src="Images/revertitparrot.gif" alt="revertitparrot.gif" />;
+   jogo[12] = <img src="Images/tripletsparrot.gif" alt="tripletsparrot.gif" />;
+   jogo[13] = <img src="Images/unicornparrot.gif" alt="unicornparrot.gif" />;
+
+   const main = document.querySelector("main");
+   for (let i = 0; i < cartas.length; i++) {
+      main.innerHTML = main.innerHTML + `{jogo[i]}`;
+
+   }*/
