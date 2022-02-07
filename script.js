1dbb276be11a4719d8744b7a2e4eee9410fdfaa5
Ana Raquel <anaraquelpmatos@gmail.com>
2022-02-07 13:39:01 -0300

    Funções adicionadas: pergunta ao usuário e virada de cartas

--- script.js
+++ script.js
@@ -1,33 +1,115 @@
-/*let cartas = null;
-let jogo = [];
+let cards;
+const all = [];
+let cardHTML;
+let closeCard = false;
+const backGame = [
+   'bobrossparrot.gif',
+   'explodyparrot.gif',
+   'fiestaparrot.gif',
+   'metalparrot.gif',
+   'revertitparrot.gif',
+   'tripletsparrot.gif',
+   'unicornparrot.gif'
+]
 
-function perguntarUsuario() {
- 
-   while ((cartas % 2 !== 0) || (cartas < 4)|| (cartas > 14) || (cartas === "") || (cartas === null)) {
-      cartas = parseInt(prompt("Olá! Quantas cartas você deseja para iniciar o jogo? Escolha um número par de 1 a 14!"));
-    }
+function askUser() {
+
+   while ((cards % 2 !== 0) || (cards < 4) || (cards > 14) || (cards === "") || (cards === null)) {
+      cards = parseInt(prompt("Olá! Quantas cartas você deseja para iniciar o jogo? Escolha um número par de 1 a 14!"));
+   }
 }
-perguntarUsuario()
+askUser()
 
-/*
-function selecionarCartas() {
-   jogo[0] = <img src="Images/bobrossparrot.gif" alt="bobrossparrot.gif" />;
-   jogo[1] = <img src="Images/metalparrot.gif" alt="metalparrot.gif" />;
-   jogo[2] = <img src="Images/explodyparrot.gif" alt="explodyparrot.gif" />;
-   jogo[3] = <img src="Images/fiestaparrotr.gif" alt="fiestaparrotr.gif" />;
-   jogo[4] = <img src="Images/revertitparrot.gif" alt="revertitparrot.gif" />;
-   jogo[5] = <img src="Images/tripletsparrot.gif" alt="tripletsparrot.gif" />;
-   jogo[6] = <img src="Images/unicornparrot.gif" alt="unicornparrot.gif" />;
-   jogo[7] = <img src="Images/bobrossparrot.gif" alt="bobrossparrot.gif" />;
-   jogo[8] = <img src="Images/metalparrot.gif" alt="metalparrot.gif" />;
-   jogo[9] = <img src="Images/explodyparrot.gif" alt="explodyparrot.gif" />;
-   jogo[10] = <img src="Images/fiestaparrotr.gif" alt="fiestaparrotr.gif" />;
-   jogo[11] = <img src="Images/revertitparrot.gif" alt="revertitparrot.gif" />;
-   jogo[12] = <img src="Images/tripletsparrot.gif" alt="tripletsparrot.gif" />;
-   jogo[13] = <img src="Images/unicornparrot.gif" alt="unicornparrot.gif" />;
+function selectCards() {
 
-   const main = document.querySelector("main");
-   for (let i = 0; i < cartas.length; i++) {
-      main.innerHTML = main.innerHTML + `{jogo[i]}`;
 
-   }*/
+   for (let i = 0; i < cards / 2; i++) {
+
+      cardHTML = `
+      <div class = "card">
+      <img class = "front-end" src="Images/front.png" data-card="${backGame[i]}"/>
+      <img class = "back-end" onclick="clickCards()" src="Images/${backGame[i]}"/>
+      </div>`
+
+      all.push(cardHTML);
+      all.push(cardHTML);
+
+   }
+
+   all.sort(compare);
+
+
+   for (i = 0; i < all.length; i++) {
+
+      const card = document.querySelector(".all-cards");
+      card.innerHTML += all[i];
+   }
+
+}
+
+function compare() {
+   return Math.random() - 0.5;
+}
+
+
+selectCards()
+
+const changes = document.querySelectorAll(".card");
+
+let firstCard, secondCard;
+
+function clickCards() {
+   if (closeCard === true) return false;
+  this.classList.add("flip");
+
+   if (!firstCard) {
+      firstCard = this;
+
+      return false;
+   }
+
+   if (!secondCard) {
+      secondCard = this;
+
+      return false;
+
+   }
+
+   searchMatch();
+}
+
+
+function searchMatch() {
+   let = match = firstCard === secondCard;
+
+
+  if (match) {
+
+    returnCards();
+
+   }else{
+      cleanCards(match);
+ }
+
+}
+
+function returnCards() {
+   closeCard = true;
+   setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+      cleanCards();
+   }, 1000);
+
+
+}
+
+function cleanCards(match = false) {
+
+   if (match) {
+      firstCard.removeEventListener('click', clickCards);
+      secondCard.removeEventListener('click', clickCards);
+   }
+   [firstCard, secondCard, closeCard] = [null, null, false];
+}
+changes.forEach(card => card.addEventListener('click', clickCards));
